256ba364f0ed280c0dd85558e99bb3d18dd286a9
tv00iPapa <dpilguev@yandex.com>
2026-09-07 15:28:03 -0400

    The start of the icmp implementation.

--- c2_server/icmp_listener.py
+++ c2_server/icmp_listener.py
@@ -1,2 +1,28 @@
+import socket
+import struct
+
+def checksum(data):
+    summ = 0
+    
+    i = 0
+    while i + 1 < len(data):
+        word = struct.unpack_from("!H", data, i)[0]
+        summ += word
+        i += 2
+
+    if i < len(data):
+        summ += data[i]
+
+    while summ >> 16:
+        summ = (summ & 0xffff) + (summ >> 16)
+
+    return (~summ) & 0xffff
+
 def run_icmp(stop_event, task_manager):
-    pass
+    print("[+/ICMP] icmp-слушатель работает");
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    while not stop_event.is_set():
+        pass
+
+    print("[+/ICMP] icmp-слушатель прекратил работу.")
